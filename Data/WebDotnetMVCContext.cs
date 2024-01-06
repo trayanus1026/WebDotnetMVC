0df3fd2758b56b332ae2be1f3d72ad19bc69e2c3
trayanus <trayanus1026@gmail.com>
2024-01-06 01:20:55 +0100

    first commit

---
+++ Data/WebDotnetMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebDotnetMVC.Models;
+
+namespace WebDotnetMVC.Data
+{
+    public class WebDotnetMVCContext : DbContext
+    {
+        public WebDotnetMVCContext (DbContextOptions<WebDotnetMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebDotnetMVC.Models.Movie> Movie { get; set; } = default!;
+    }
+}
